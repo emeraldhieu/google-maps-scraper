41d4c3fdddaca0f164ad3b57a58683bb986d3be4
Hieu <emerald.hieu@gmail.com>
2023-01-12 15:02:13 +0700

    Conform to PEP8 (Python Enhancement Proposal 8)
    * See https://peps.python.org/pep-0008/

--- app.py
+++ app.py
@@ -6,108 +6,128 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 import json
 
-def scrollToBottom(driver):
-   # Find the container of result elements
-   scrollableContainer = getScrollableContainer(driver)
 
-   # Find the bottom panel if exists
-   bottomPanel = getBottomPanel(driver)
+def scroll_to_bottom(driver):
+    # Find the container of result elements
+    scrollable_container = get_scrollable_container(driver)
 
-   ordinate = 0
-   scrollHeight = getScrollHeight(scrollableContainer)
-   previousPlaceFeedCount = 0
-   while bottomPanel is None:
-      placeFeedCount = getPlaceFeedCount(driver)
-      print("previousPlaceFeedCount: " + str(previousPlaceFeedCount))
-      print("placeFeedCount: " + str(placeFeedCount))
-      print("ordinate: " + str(ordinate))
+    # Find the bottom panel if exists
+    bottom_panel = get_bottom_panel(driver)
 
-      # If the loading is not responsive, scroll up then scroll down to send another request
-      if placeFeedCount == previousPlaceFeedCount:
-         ordinate -= scrollHeight
-         scrollTop(scrollableContainer, ordinate)
-         time.sleep(2)
-      
-      ordinate += scrollHeight
-      scrollTop(scrollableContainer, ordinate)
-      time.sleep(2)
-      bottomPanel = getBottomPanel(driver)
-      previousPlaceFeedCount = placeFeedCount
+    ordinate = 0
+    scroll_height = get_scroll_height(scrollable_container)
+    previous_place_feed_count = 0
+    while bottom_panel is None:
+        place_feed_count = get_place_feed_count(driver)
+        print("previous_place_feed_count: " + str(previous_place_feed_count))
+        print("place_feed_count: " + str(place_feed_count))
+        print("ordinate: " + str(ordinate))
 
-def getScrollHeight(scrollableContainer):
-   return driver.execute_script("return arguments[0].scrollHeight", scrollableContainer)
+        # If the loading is not responsive, scroll up then scroll down to send another request
+        if place_feed_count == previous_place_feed_count:
+            ordinate -= scroll_height
+            scroll_top(scrollable_container, ordinate)
+            time.sleep(2)
 
-def getSearchInput(driver):
-   return getElementFromDriver(driver, "#searchboxinput");
+        ordinate += scroll_height
+        scroll_top(scrollable_container, ordinate)
+        time.sleep(2)
+        bottom_panel = get_bottom_panel(driver)
+        previous_place_feed_count = place_feed_count
 
-def getScrollableContainer(driver):
-   return getElementFromDriver(driver, ".m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd[role='feed']")
 
-def getBottomPanel(driver):
-   return getElementFromDriver(driver, ".m6QErb.tLjsW.eKbjU");
+def get_scroll_height(scrollable_container):
+    return driver.execute_script("return arguments[0].scrollHeight", scrollable_container)
 
-def getPlaceFeedCount(driver):
-   return len(getPlaceFeeds(driver))
 
-def getPlaceFeeds(driver):   
-   elements = getElementsFromDriver(driver, ".m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd[role='feed'] > div:has(> div[role='article'])")
-   return elements if elements is not None else []
+def get_search_input(driver):
+    return get_element_from_driver(driver, "#searchboxinput");
 
-def scrollTop(scrollableContainer, ordinate):
-   driver.execute_script("arguments[0].scrollTop = arguments[1]", scrollableContainer, ordinate)
 
-def getElementFromDriver(driver, selector):
-   try:
-      element = driver.find_element(By.CSS_SELECTOR, selector)
-      return element
-   except NoSuchElementException:
-      pass
+def get_scrollable_container(driver):
+    return get_element_from_driver(driver, ".m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd[role='feed']")
 
-def getElementsFromDriver(driver, selector):
-   try:
-      elements = driver.find_elements(By.CSS_SELECTOR, selector)
-      return elements
-   except NoSuchElementException:
-      pass
 
-def getElement(parent, selector):
-   try:
-      element = parent.find_element(By.CSS_SELECTOR, selector)
-      return element
-   except NoSuchElementException:
-      pass
+def get_bottom_panel(driver):
+    return get_element_from_driver(driver, ".m6QErb.tLjsW.eKbjU");
 
-def getPlaceName(placeFeed):
-   placeArticle = getElement(placeFeed, "div[role='article']")
-   return placeArticle.get_attribute("aria-label");
 
-def getStars(placeFeed):
-   element = getElement(placeFeed, ".UaQhfb :nth-child(3) .AJB7ye .ZkP5Je .MW4etd")
-   if element is None:
-      return ""
-   return element.text
+def get_place_feed_count(driver):
+    return len(get_place_feeds(driver))
 
-def getReviews(placeFeed):
-   element = getElement(placeFeed, ".UaQhfb :nth-child(3) .AJB7ye .ZkP5Je .UY7F9")
-   if element is None:
-      return ""
-   return element.text[1:-1]
-   
-def getAddress(placeFeed):
-   element = getElement(placeFeed, ".UaQhfb > :nth-child(4) > :nth-child(2) > :nth-child(2) > jsl > :nth-child(2)")
-   if element is None:
-      return ""
-   return element.text
 
-def getPhoneNumber(placeFeed):
-   element = getElement(placeFeed, ".UaQhfb > :nth-child(4) > :nth-child(4) > :nth-child(2) > jsl > :nth-child(2)")
-   if element is None:
-      return ""
-   return element.text
+def get_place_feeds(driver):
+    elements = get_elements_from_driver(driver,
+                                        ".m6QErb.DxyBCb.kA9KIf.dS8AEf.ecceSd[role='feed']"
+                                        " > div:has(> div[role='article'])")
+    return elements if elements is not None else []
 
-def writeToFile(filePath, places):
-   with open(filePath, 'w') as outputFile:
-      json.dump(places, outputFile)   
+
+def scroll_top(scrollable_container, ordinate):
+    driver.execute_script("arguments[0].scrollTop = arguments[1]", scrollable_container, ordinate)
+
+
+def get_element_from_driver(driver, selector):
+    try:
+        element = driver.find_element(By.CSS_SELECTOR, selector)
+        return element
+    except NoSuchElementException:
+        pass
+
+
+def get_elements_from_driver(driver, selector):
+    try:
+        elements = driver.find_elements(By.CSS_SELECTOR, selector)
+        return elements
+    except NoSuchElementException:
+        pass
+
+
+def get_element(parent, selector):
+    try:
+        element = parent.find_element(By.CSS_SELECTOR, selector)
+        return element
+    except NoSuchElementException:
+        pass
+
+
+def get_place_name(placeFeed):
+    place_article = get_element(placeFeed, "div[role='article']")
+    return place_article.get_attribute("aria-label");
+
+
+def get_stars(place_feed):
+    element = get_element(place_feed, ".UaQhfb :nth-child(3) .AJB7ye .ZkP5Je .MW4etd")
+    if element is None:
+        return ""
+    return element.text
+
+
+def get_reviews(place_feed):
+    element = get_element(place_feed, ".UaQhfb :nth-child(3) .AJB7ye .ZkP5Je .UY7F9")
+    if element is None:
+        return ""
+    return element.text[1:-1]
+
+
+def get_address(place_feed):
+    element = get_element(place_feed, ".UaQhfb > :nth-child(4) > :nth-child(2) > :nth-child(2) > jsl > :nth-child(2)")
+    if element is None:
+        return ""
+    return element.text
+
+
+def get_phone_number(place_feed):
+    element = get_element(place_feed, ".UaQhfb > :nth-child(4) > :nth-child(4) > :nth-child(2) > jsl > :nth-child(2)")
+    if element is None:
+        return ""
+    return element.text
+
+
+def write_to_file(file_path, places):
+    with open(file_path, 'w') as output_file:
+        json.dump(places, output_file)
+
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -115,64 +135,51 @@ url = "https://maps.google.com"
 driver.get(url)
 
 # Find the Search bar
-searchInput = getSearchInput(driver)
+search_input = get_search_input(driver)
 
 # Enter a search text
-searchText = "Surf shops in Florida"
-searchInput.send_keys(searchText)
+search_text = "Surf shops in Florida"
+search_input.send_keys(search_text)
 
 # Press Return
-searchInput.send_keys(Keys.RETURN)
+search_input.send_keys(Keys.RETURN)
 
 # Wait for the search results to appear
 time.sleep(5)
 
 # Scroll down until the bottom panel is found
-scrollToBottom(driver)
- 
+scroll_to_bottom(driver)
+
 # Prepare JSON result
-places = []; 
+places = [];
 
 # Find the top places displayed in the search result
-placeFeeds = getPlaceFeeds(driver)
+place_feeds = get_place_feeds(driver)
 
-for placeFeed in placeFeeds:
-   placeObj = {};
+for place_feed in place_feeds:
+    place_obj = {};
 
-   # Get the place article wrapped inside the place feed.
-   placeName = getPlaceName(placeFeed)
-   placeObj["name"] = placeName
+    # Get the place article wrapped inside the place feed.
+    place_name = get_place_name(place_feed)
+    place_obj["name"] = place_name
 
-   stars = getStars(placeFeed)
-   placeObj["stars"] = stars
+    stars = get_stars(place_feed)
+    place_obj["stars"] = stars
 
-   reviews = getReviews(placeFeed)
-   placeObj["reviews"] = reviews
-      
-   address = getAddress(placeFeed)
-   placeObj["address"] = address
+    reviews = get_reviews(place_feed)
+    place_obj["reviews"] = reviews
 
-   # TODO Investigate why this element is None though I can get the element in Chrome's developer's console
-   #phoneNumber = getPhoneNumber(placeFeed)
-   #placeObj["phoneNumber"] = phoneNumber
+    address = get_address(place_feed)
+    place_obj["address"] = address
 
-   # Store place
-   places.append(placeObj);
+    # TODO Investigate why this element is None though I can get the element in Chrome's developer's console
+    phone_number = get_phone_number(place_feed)
+    place_obj["phoneNumber"] = phone_number
 
-filePath = "places.json"
-writeToFile(filePath, places)
+    # Store place
+    places.append(place_obj);
 
+file_path = "places.json"
+write_to_file(file_path, places)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("DONE")
