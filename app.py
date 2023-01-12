474f5a1bba3728494113079af2db943bd87b0136
Hieu <emerald.hieu@gmail.com>
2023-01-12 19:04:32 +0700

    Document steps in code

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from scraper.scraper import Scraper
 
 
 def run():
-    scraper = Scraper()
+    # 1) Pass headless=True if you want to run the browser in background
+    scraper = Scraper(headless=False)
+
+    # 2) Pass the text you want to search for
     search_text = "Surf shops in Florida"
+
+    # 3) Pass the file path you want to export into
     file_path = "places.json"
+
     scraper.scrape(search_text, file_path)
     print("DONE")
 
